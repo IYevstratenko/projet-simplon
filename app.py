f1cd14e8bac4aaa18bae1ceb8f01a035b514f955
IYevstratenko <i.a.yevstratenko@gmail.com>
2025-08-01 12:31:18 +0000

    Ajout des requêtes SQL terminées

--- app.py
+++ app.py
@@ -4,7 +4,15 @@ import pandas as pd
 données = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSC4KusfFzvOsr8WJRgozzsCxrELW4G4PopUkiDbvrrV2lg0S19-zeryp02MC9WYSVBuzGCUtn8ucZW/pub?output=csv')
 
 figure = px.pie(données, values='qte', names='region', title='quantité vendue par région')
-
 figure.write_html('ventes-par-region.html')
 
-print('ventes-par-région.html généré avec succès !')
+fig2 = px.bar(données.groupby('produit', as_index=False).agg({'qte': 'sum'}),
+              x='produit', y='qte', title='Ventes par produit')
+fig2.write_html("ventes-par-produit.html")
+
+données['ca'] = données['prix'] * données['qte']
+fig3 = px.bar(données.groupby('produit', as_index=False).agg({'ca': 'sum'}),
+              x='produit', y='ca', title="Chiffre d'affaires par produit")
+fig3.write_html("chiffre-affaires-par-produit.html")
+
+print('Tous les graphiques ont été générés avec succès !')
